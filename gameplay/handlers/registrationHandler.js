ba46aea4b78bb883c68b111c28c6cc9664a5b7a1
Dan T <dantu22@live.com>
2019-04-12 11:28:23 -0700

    Refactor registration handler code for reuse

--- gameplay/handlers/registrationHandler.js
+++ gameplay/handlers/registrationHandler.js
@@ -3,36 +3,66 @@
 const logger = require('winston');
 const db = require('../../db/db');
 
-module.exports = (psid, args) => {
+// Checks if the user's PSID is registered.
+// If it is, calls the exists function
+// If not, calls the does not exist (dne) function
+// If checking the database fails, calls fail
+let checkUserRegistered = (psid, username, userExists, userDNE, fail) => {
+    let user_exists_query = sql_commands.CHECK_USER_EXISTS;
+    db.all(user_exists_query, [psid], (err, rows) => {
+        if (err) {
+            logger.error("Failed to check database for existing user: " + err);
+            fail();
+            return;
+        }
+        // Check query results for existing user
+        if (rows.length !== 0 && rows[0].psid == psid) {
+            userExists(psid, rows[0].username);
+        } else {
+            userDNE(psid, username, fail);
+        } 
+    });
+}
+
+// Adds a new user with a given username
+// Only called when user uses register command
+let registerUser = (psid, username, fail) => {
+    let add_user = sql_commands.REGISTER_NEW_USER;
+    db.run(add_user, [psid, username], (err) => {
+        if (err) {
+            logger.error("Error adding new player to database: " + err);
+            fail();
+            return;
+        }
+        logger.info("New user registered: PSID " + psid + " -> " + username);
+        sendResponse(psid, "You are now registered under the username *" + username + "*. Enjoy the game!");
+    });
+}
+
+// Handles the register command
+let handleRegistration = (psid, args) => {
+    // Check for correct usage
     if (args.length !== 1) {
         sendResponse(psid, "Please use *Register <Username>* to register. Usernames may not have any spaces in them.");
         return;
     }
 
-    // See if user is already registered
-    let user_exists_query = sql_commands.CHECK_USER_EXISTS;
-    db.all(user_exists_query, [psid], (err, rows) => {
-        if (err) {
-            logger.error("Error checking database for existing user: " + err);
-            sendResponse(psid, "I couldn't make an account for you. Please try again in a few minutes.");
-            return;
-        }
+    let handleUserExists = (psid, username) => {
+        sendResponse(psid, "You are already registered with the name *" + username + "*");
+    }
 
-        // If user already exists in our DB
-        if (rows.length !== 0 && rows[0].psid == psid) {
-            sendResponse(psid, ("You are already registered with the username: *" + rows[0].ign + "*"));
-        } else {
-            // There are no entries for that PSID, register a new user
-            let add_user = sql_commands.REGISTER_NEW_USER;
-            db.run(add_user, [psid, args[0]], (err) => {
-                if (err) {
-                    logger.error("Error adding new player to database: " + err);
-                    sendResponse(psid, "I couldn't make an account for you. Please try again in a few minutes.");
-                    return;
-                }
-                logger.info("New user registered. PSID: " + psid + " -> " + args[0]);
-                sendResponse(psid, "You are now registered under the username *" + args[0] + "*. Enjoy the game!");
-            });
-        }
-    });
+    // When db queries fail
+    let handleErrors = () => {
+        sendResponse(psid, "I couldn't do what you wanted! Please try again in a few minutes.")
+    }
+
+    checkUserRegistered(psid, args[0], handleUserExists, registerUser, handleErrors);
 }
+
+module.exports = {
+    handleRegistration: handleRegistration,
+    checkUserRegistered: checkUserRegistered
+}
+
+
+
--- gameplay/json/sql_commands.js
+++ gameplay/json/sql_commands.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "CHECK_DB_EXISTS" : "CREATE TABLE IF NOT EXISTS players (psid text primary key, ign text, level int, exp int, max_hp, int, damage int, defense int)",
+    "CHECK_DB_EXISTS" : "CREATE TABLE IF NOT EXISTS players (psid text primary key, username text, level int, exp int, max_hp, int, damage int, defense int)",
     "CHECK_USER_EXISTS" : "SELECT * FROM players WHERE psid = (?)",
     "REGISTER_NEW_USER" : "NSERT INTO players VALUES ((?), (?), 1, 0, 10, 5, 5)",
 };
